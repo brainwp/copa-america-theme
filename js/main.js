9cc664926f7bf69d6ba704d33514efa58d0faa8d
everaldomatias <matiaseveraldo@gmail.com>
2015-05-20 07:00:17 -0300

    Desliga NiceScroll

--- js/main.js
+++ js/main.js
@@ -4,6 +4,6 @@ $(document).ready(function() {
   	if($('body').hasClass('page-template-default') || $('body').hasClass('page-template-page-times')){
   		$('.bg-slider').css('height',$('.entry-header-page').outerHeight(true) + 'px');
   	}
-  	$("html").niceScroll();
+  	//$("html").niceScroll();//
 });	
 });
